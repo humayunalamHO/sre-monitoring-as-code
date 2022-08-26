21f755c737d97b44f81ec4918d3111a2f9e93204
Humayun Alam <humayun.alam@digital.homeoffice.gov.uk>
2022-08-26 17:00:12 +0100

    github action

--- monitoring-as-code/tools/promtool/prom.sh
+++ monitoring-as-code/tools/promtool/prom.sh
@@ -1,1 +1,1 @@
-curl -Ls https://api.github.com/repos/prometheus/prometheus/releases/latest | jq ".tag_name" | xargs | cut -c2-
+VERSION=$(curl -Ls https://api.github.com/repos/prometheus/prometheus/releases/latest | jq ".tag_name" | xargs | cut -c2-)
